3bff19e69d197025c11ea93fccb2d378e08d53e6
Karol Wójcik <blog@karolwojcik.net>
2018-02-20 22:37:02 +0100

    tutorial movie #1

---
+++ Assets/ZombieCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieCtrl : MonoBehaviour {
+
+	public float zombieSpeed;
+
+	void Start () {
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, Vector3.zero - transform.position) * Quaternion.Euler(0,0,90);
+	}
+	
+
+	void Update () {
+		transform.position = Vector2.MoveTowards (new Vector2 (transform.position.x, transform.position.y), Vector2.zero, zombieSpeed * Time.deltaTime);
+	}
+}
